eb959a362738439460d0bc63594eae32228847d1
Chris Granger <ibdknox@gmail.com>
2011-10-08 10:01:27 -0700

    Edited project.clj via GitHub

--- project.clj
+++ project.clj
@@ -2,7 +2,5 @@
   :description "A leiningen plugin to launch a vimclojure nailgun server."
   :url "http://github.com/mrowl/lein-nailgun"
   :dependencies [[org.clojars.gilbertl/vimclojure "2.1.2"]
-                 [vimclojure/server "2.2.0"]]
-  :dev-dependencies [[lein-clojars "0.7.0"]]
-  :repositories {"clojars" "http://clojars.org/repo"})
+                 [vimclojure/server "2.3.0"]])
 
